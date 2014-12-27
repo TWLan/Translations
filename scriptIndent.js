44bfc0320f45a7b224dc3313c4e092cf9997f597
steffen <steffenb198@aol.com>
2014-12-27 18:05:36 +0100

    Add php scripts to indent/unindent,hope this works without unexpected fail

--- scriptIndent.js
+++
@@ -1,7 +0,0 @@
-/* Convert all json files to indented versions (for being commited/editable) */
-
-var fs = require('fs');
-fs.readdirSync('.').forEach(function (file) {
-    if (file.substr(-5) != ".json") return;
-    fs.writeFileSync(file, JSON.stringify(JSON.parse(fs.readFileSync(file, {encoding: "UTF-8"})), null, 4));
-});
--- scriptUnindent.js
+++
@@ -1,6 +0,0 @@
-/* Convert all json files to releasable json files, which are not indented */
-var fs = require('fs');
-fs.readdirSync('.').forEach(function (file) {
-    if (file.substr(-5) != ".json") return;
-    fs.writeFileSync(file, JSON.stringify(JSON.parse(fs.readFileSync(file, {encoding: "UTF-8"}))));
-});
