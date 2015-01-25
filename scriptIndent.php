85a85b91699f0707e05710d9d8ddb63f98a70c29
steffen <steffenb198@aol.com>
2015-01-25 18:52:34 +0100

    Sort keys, add lang entries for some ally member stuff

--- scriptIndent.php
+++ scriptIndent.php
@@ -2,5 +2,7 @@
 $files = glob("*.json");
 foreach($files as $file)
 {
-    file_put_contents($file, json_encode(json_decode(file_get_contents($file)), JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+    $arr = json_decode(file_get_contents($file), true);
+    ksort($arr);
+    file_put_contents($file, json_encode($arr, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
 }
